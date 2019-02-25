7e67cc56b4a67bc46663fe2196ff7b89ee31e913
sortofbusy <sortofbusy@gmail.com>
2019-02-25 14:24:04 -0800

    added icons, tech details

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>SORTOFBUSY</strong> <span></span></Link>
+        <Link to="/" className="logo"><strong>SORTOFBUSY</strong></Link>
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
         </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -31,29 +31,55 @@ class HomeIndex extends React.Component {
                     <section id="one" className="tiles">
                         <article style={{backgroundImage: `url(${pic01})`}}>
                             <header className="major">
-                                <h3>Contact Bridger</h3>
+                                <h3><span className="fa fa-android icon-bg"></span> Contact Bridger</h3>
+                                <p>
+                                    <ul className="horizontal-list">
+                                        <li>React Native</li>
+                                        <li>Custom Native Module</li>
+                                    </ul>
+                                </p>
                                 <p>Android app that imports contacts from a Google Sheet</p>
                             </header>
                             <Link to="/contact-bridger" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic02})`}}>
                             <header className="major">
-                                <h3>Eat the Bible</h3>
+                                <h3><span className="fa fa-mobile-phone"></span> Eat the Bible</h3>
+                                <p>
+                                    <ul className="horizontal-list">
+                                        <li>Firebase</li>
+                                        <li>React</li>
+                                        <li>SPA</li>
+                                    </ul>
+                                </p>
                                 <p>Serverless Bible reading management site with highly customizable plans and note-taking capabilities</p>
                             </header>
                             <Link to="/eat-the-bible" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic03})`}}>
                             <header className="major">
-                                <h3>Northwest Training</h3>
+                                <h3><span className="fa fa-code"></span> Northwest Training</h3>
+                                <p>
+                                    <ul className="horizontal-list">
+                                        <li>MEAN stack</li>
+                                        <li>SPA</li>
+                                    </ul>
+                                </p>
                                 <p>MEAN stack event registration and management site</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic04})`}}>
                             <header className="major">
-                                <h3>Quikitech.com <span className="icon fa fa-external-link"></span></h3>
-                                <p>Wordpress/WooCommerce</p>
+                                <h3><span className="fa fa-globe"></span> Quikitech.com <span className="icon fa fa-external-link"></span></h3>
+                                <p>
+                                    <ul className="horizontal-list">
+                                        <li>Wordpress</li>
+                                        <li>Woocommerce</li>
+                                        <li>Moodle</li>
+                                    </ul>
+                                </p>
+                                <p>E-learning for teachers</p>
                             </header>
                             <a href="https://quikitech.com" className="link primary"></a>
                         </article>
